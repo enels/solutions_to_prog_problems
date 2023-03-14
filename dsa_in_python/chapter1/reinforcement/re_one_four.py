c72a54e2367921fdccc3f964c39bb88f1a771706
enels <enomauwaifo@gmail.com>
2023-03-14 17:01:31 +0100

    function that sums squares of numbers less than the given positive integer

---
+++ dsa_in_python/chapter1/reinforcement/re_one_four.py
@@ -0,0 +1,14 @@
+def sum_of_squares(n):
+    '''
+    Takes a positive integer n and find the sum of all positive integer
+    less than n
+    :param n: positive argument
+    :return: sum of squares of numbers less than n
+    '''
+
+    sum = 0
+    for num in range(n):
+        if num < n:
+            sum += num ** 2
+
+    return sum
