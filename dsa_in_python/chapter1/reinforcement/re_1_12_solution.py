4fbb1a9d201df208f0088b760ecfa90af81d4d16
enels <enomauwaifo@gmail.com>
2023-03-18 19:45:35 +0100

    create custom version of python choice function

---
+++ dsa_in_python/chapter1/reinforcement/re_1_12_solution.py
@@ -0,0 +1,20 @@
+from random import randrange
+
+def my_choice(data):
+    # get the max and min value of data
+    # use randrange to iterate thru it with min and max as stop and start resp.
+    # check if each value returned by randrange is in data
+    # return the first one found
+    '''
+    Implementation of a custom version of python's choice
+    :param data: the data to perform the choice function on
+    :return: the first random value found
+    '''
+
+    min_value = min(data)
+    max_value = max(data)
+
+    while True:
+        result = randrange(min_value, max_value)
+        if result in data:
+            return result
