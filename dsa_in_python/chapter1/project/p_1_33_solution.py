9a10c8e694c8137ffffa41a2ad7fec6c4d80b590
enels <enomauwaifo@gmail.com>
2023-04-07 19:22:30 +0100

    Program to simulate a handheld calculator

---
+++ dsa_in_python/chapter1/project/p_1_33_solution.py
@@ -0,0 +1,57 @@
+def calculate(rvalue, op, lvalue):
+    '''
+        perform the calculation based on the operation - op
+    :param rvalue: right value
+    :param op: operation to carry out
+    :param lvalue: left value
+    :return: integer of float
+    '''
+
+    if op == "+":
+        return rvalue + lvalue
+    elif op == "-":
+        return rvalue - lvalue
+    elif op == "*":
+        return rvalue * lvalue
+    elif op == "/":
+        return rvalue / lvalue
+
+def handheld_calculator():
+    '''
+        program that simulates a simple handheld calculator
+    :return: result obtained
+    '''
+
+    # set the possible operators
+    ops = ["+", "-", "*", "/", "="]
+
+    # container for storing user's input
+    user_input = list()
+
+    # get the user's inputs - values and operators
+    while True:
+        user_input = input(": ")
+
+        # split into list
+        user_input = user_input.split()
+        # alphabet keyed in
+        if "r" in user_input or "c" in user_input:
+            user_input.clear()
+            continue
+        break
+
+    user_input_len = len(user_input)
+
+    # get initial result
+    result = calculate(int(user_input[0]), user_input[1], int(user_input[2]))
+
+    # perform calculation
+    for i in range(3, user_input_len):
+        # get user operator
+        if user_input[i] == "=":
+            return result
+        op = user_input[i]
+        if op in ops:
+            result = calculate(result, user_input[i], int(user_input[i+1]))
+
+    return result
