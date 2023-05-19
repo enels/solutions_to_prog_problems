559edf61acf03a6bf3ea4e01c4e66b8a6cc8240d
enels <enomauwaifo@gmail.com>
2023-05-19 09:37:28 +0100

    put the driver section

--- dsa_in_python/chapter1/reinforcement/re_one_five.py
+++ dsa_in_python/chapter1/reinforcement/re_one_five.py
@@ -1,6 +1,6 @@
 def sum_of_squares(n):
     '''
-    Takes a positive integer n and find the sum of all positive integer
+    Takes a positive integer n and find the sum of squares of all positive integer
     less than n using list python's comprehension
     :param n: positive argument
     :return: sum of squares of numbers less than n
@@ -8,3 +8,6 @@ def sum_of_squares(n):
 
     result = sum([num**2 for num in range(n)])
     return result
+
+if __name__ == "__main__":
+    print(sum_of_squares(4))
