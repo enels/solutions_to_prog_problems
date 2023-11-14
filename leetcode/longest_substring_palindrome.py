da3bd153667c3f193b4202f9ed3070f7234afba6
Enoma Uwaifo <enomauwaifo@gmail.com>
2023-11-14 09:37:02 +0100

    add further testcases with single and double char str

--- leetcode/longest_substring_palindrome.py
+++ leetcode/longest_substring_palindrome.py
@@ -1,49 +1,53 @@
-def longestPalindrome(s):
-    """
-    :type s: str
-    :rtype: str
-    """
+class Solution(object):
+    def longestPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
 
-    # length of string
-    s_len = len(s)
+        # one character string
+        if len(s) == 1 or len(s) == 2 and s[0] != s[1]:
+            return s[0]
 
-    # stores current maximum length of sub palindrome
-    max_len_sub_palin = 0
-    max_palin_str = max_sub_palin = ""
+        # length of strin
+        s_len = len(s)
 
+        # stores current maximum length of sub palindrome
+        max_len_sub_palin = 0
+        max_palin_str = max_sub_palin = ""
 
-    for n in range(s_len-1):
-        start_c = s[n]
-        inner_c = ""
-        for c in s[n + 1:]:
-            if c != start_c:
-                inner_c += c
-            else:
-                inner_c += c # last character encountered
-                start_c += inner_c
+        for i in range(s_len - 1):
+            start_c = s[i]
+            inner_c = ""
+            for c in s[i + 1:]:
+                if c != start_c:
+                    inner_c += c
+                else:
+                    inner_c += c  # last character encountered
+                    start_c += inner_c
 
-                # check if it's a palindrome
-                start_c_len = len(start_c)
-                mid = start_c_len // 2
+                    # check if it's a palindrome
+                    start_c_len = len(start_c)
+                    mid = start_c_len // 2
 
-                # assumgin it's a palindrome
-                is_palindrome = True
-                for n in range(mid):
-                    start_c_len -= 1
-                    if start_c[n] == start_c[start_c_len] and is_palindrome:
-                        pass
-                    else:
-                        is_palindrome = False
-                        break
+                    # assigning it's a palindrome
+                    is_palindrome = True
+                    for n in range(mid):
+                        start_c_len -= 1
+                        if start_c[n] == start_c[start_c_len] and is_palindrome:
+                            pass
+                        else:
+                            is_palindrome = False
+                            break
 
-                if max_len_sub_palin < len(start_c) and is_palindrome:
-                    max_palin_str = start_c
-                    max_len_sub_palin = len(start_c)
+                    if max_len_sub_palin < len(start_c) and is_palindrome:
+                        max_palin_str = start_c
+                        max_len_sub_palin = len(start_c)
 
-                # reassign it to the beginning of the current substring
-                start_c = s[n]
+                    # reassign it to the beginning of the current substring
+                    start_c = s[i]
 
-    return max_palin_str
+        if len(max_palin_str) == 0:
+            return s[0]
 
-if __name__ == "__main__":
-    print(longestPalindrome("bddaadddaaccbbb"))
+        return max_palin_str
