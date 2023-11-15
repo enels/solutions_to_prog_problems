b554151199d61eea986654696ab223daa727c5fa
Enoma Uwaifo <enomauwaifo@gmail.com>
2023-11-15 15:01:47 +0100

    right contents for re_5.1 and re_5.3

--- dsa_in_python/chapter5/reinforcement/re_5.3_solution.py
+++ dsa_in_python/chapter5/reinforcement/re_5.3_solution.py
@@ -1,14 +1,18 @@
 import sys
 data = []
 n = 10
-
 for k in range(n):
     a = len(data)
     b = sys.getsizeof(data)
-    # pop out the value when index is a factor of 3
     print('Length: {0:3d}; Size in bytes: {1:4d}'.format(a, b))
-    if k % 3 == 0 and len(data) != 0:
-        data.pop()
-        b = sys.getsizeof(data)
-        print('After popping: Length: {0:3d}; Size in bytes: {1:4d}'.format(a, b))
     data.append(None)
+    # pops out the last value when k is even
+    # to check the underlying array size
+    if k % 2 == 0 and k != 0:
+        data.pop()
+        data.pop()
+        a = len(data)
+        b = sys.getsizeof(data)
+        print('After removing an element - Length: {0:3d}; Size in bytes: {1:4d}'.format(a, b))
+        # restore back the length
+        data.append(None)
