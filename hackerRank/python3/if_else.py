0efb241d800880826f78b88149ceee4205fa0460
Enoma Uwaifo <enomauwaifo@gmail.com>
2024-11-13 07:49:54 +0100

    add comments

--- hackerRank/python3/if_else.py
+++ hackerRank/python3/if_else.py
@@ -1,6 +1,7 @@
 # covers the condition if n is odd
  if n % 2 != 0:
         print("Weird")
+    # covers the entire conditions if n is even
     elif n % 2 == 0:
         # covers the entire "Not Weird" conditions if n is even
         if (n >= 2 and n <= 5) or n > 20:
@@ -9,4 +10,3 @@
         # covers the "Weird" condition if n is even
         elif n >= 6 and n <= 20:
             print("Weird")
-    
